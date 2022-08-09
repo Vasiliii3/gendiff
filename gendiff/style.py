6e47dfb2402029f1984445d10ea8f938147c1b59
Vasilii <petgga@gmail.com>
2022-08-10 00:07:53 +0300

    fix flake8

--- gendiff/style.py
+++ gendiff/style.py
@@ -1,10 +1,9 @@
-def style_stylih(array: list)-> str:
+def style_stylih(array: list) -> str:
     level = 1
-    result = f'{{\n'
+    result = '{\n'
     for string in array:
         diff, key, value = string
         emtpy = " " * level
         result += f'{emtpy} {diff} {key}: {value}\n'
     result += "}"
     return result
-
