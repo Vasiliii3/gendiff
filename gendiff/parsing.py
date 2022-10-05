ec45bd26f3730b404a90dba12f2b5e75bf6e607c
Vasilii <petgga@gmail.com>
2022-10-05 17:52:46 +0300

    fix linter

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -3,6 +3,7 @@ from gendiff.formatter.constants import TYPE_FILE
 
 ERROR_TYPE = f'format file not supported. Supported {TYPE_FILE}'
 
+
 def parsing_type_file(file):
     path, type_file = os.path.splitext(file)
     type_fle_without_point = type_file[1:]
