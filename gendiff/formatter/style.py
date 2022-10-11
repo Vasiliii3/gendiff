061af1d7a9a39ffde912713f2a230d7d70101b1a
Vasilii <petgga@gmail.com>
2022-10-12 00:07:27 +0300

    to_str_python_to_json to to_string new result

--- gendiff/formatter/style.py
+++ gendiff/formatter/style.py
@@ -1,20 +1,20 @@
-from gendiff.formatter.constants import to_str_python_to_json
+from gendiff.formatter.constants import to_string
 from gendiff.constants import SYMBOLS
 
 
 def stylish(tree: list) -> str:
     def walk(tree_, level=1):
         result = '{\n'
+        emtpy = " " * level
         for string in tree_:
-            emtpy = " " * level
             diff, key, value = string
             diff = SYMBOLS[diff]
             if isinstance(value, list):
                 value = walk(value, level + 4)
-                value += emtpy + "   }"
             else:
-                value = to_str_python_to_json(value)
+                value = to_string(value)
             result += f'{emtpy} {diff} {key}: {value}\n'
+        result += " " * (level - 1) + "}"
         return result
 
-    return walk(tree) + "}"
+    return walk(tree)
