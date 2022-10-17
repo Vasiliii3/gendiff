2f6a1f75ea2c1d6182d012c81c4856115df1b3cf
Vasilii <petgga@gmail.com>
2022-10-17 21:48:52 +0300

    data_to_dict to parse_type_file to get_format moved get_format in file diff_files.py

--- gendiff/diff_files.py
+++ gendiff/diff_files.py
@@ -1,10 +1,13 @@
 from gendiff.io import read_file
-from gendiff.parsing import data_to_dict
+from gendiff.parsing import parse
 from gendiff.formatter.style import stylish
 from gendiff.formatter.plain import plain
 from gendiff.formatter.json import styling_json
 from gendiff.tree import walk
-from gendiff.formatter.constants import TYPE_FORMAT
+from gendiff.formatter.constants import TYPE_FORMAT, TYPE_FILE
+import os.path
+
+ERROR_TYPE = f'format file not supported. Supported {TYPE_FILE}'
 
 FORMAT = {'stylish': stylish,
           "plain": plain,
@@ -14,11 +17,19 @@ FORMAT = {'stylish': stylish,
 ERROR_FORMAT = f'format not supported. Supported {TYPE_FORMAT}'
 
 
+def get_format(file):
+    path, type_file = os.path.splitext(file)
+    type_fle_without_point = type_file[1:]
+    if type_fle_without_point not in TYPE_FILE:
+        raise Exception(ERROR_TYPE)
+    return type_fle_without_point
+
+
 def generate_diff(file_path_one, file_path_two, style: str = 'stylish') -> str:
     if style not in TYPE_FORMAT:
         raise Exception(ERROR_FORMAT)
-    data_file_one = data_to_dict(read_file(file_path_one), file_path_one)
-    data_file_two = data_to_dict(read_file(file_path_two), file_path_two)
+    data_file_one = parse(read_file(file_path_one), get_format(file_path_one))
+    data_file_two = parse(read_file(file_path_two), get_format(file_path_two))
 
     tree = walk(data_file_one, data_file_two)
 
--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -1,9 +1,5 @@
-import os.path
 import json
 import yaml
-from gendiff.formatter.constants import TYPE_FILE
-
-ERROR_TYPE = f'format file not supported. Supported {TYPE_FILE}'
 
 PARSERS = {
     "json": json.loads,
@@ -12,14 +8,5 @@ PARSERS = {
 }
 
 
-def parsing_type_file(file):
-    path, type_file = os.path.splitext(file)
-    type_fle_without_point = type_file[1:]
-    if type_fle_without_point not in TYPE_FILE:
-        raise Exception(ERROR_TYPE)
-    return type_fle_without_point
-
-
-def data_to_dict(data, file):
-    type_file = parsing_type_file(file)
+def parse(data, type_file):
     return PARSERS[type_file](data)
